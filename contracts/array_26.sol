00255e1c9a13769d66786c35beced8c5e0d42cc4
Ed-devel <edson.courses@proton.me>
2025-02-05 11:49:23 -0300

    Vídeo 27 - Remover do array por deslocamento | Como remover elemento do array em Solidity. (array_remove_by_shifting_27.sol)

--- contracts/array_26.sol
+++ contracts/array_26.sol
@@ -81,3 +81,54 @@ contract Array {
         uint[] memory a = new uint[](5);
     }
 }
+
+/*
+
+" Chamando cada função, podemos ver que o public função usa 496 gás, enquanto
+o external a função usa somente 261.
+A diferença é que, em funções públicas, o Solidity copia imediatamente
+argumentos de matriz para a memória, enquanto funções externas podem ler
+diretamente do calldata. A alocação de memória é cara, enquanto a leitura
+do calldata é barata.
+
+A razão pela qual public as funções precisam escrever todos os argumentos
+para a memória é que as funções públicas podem ser chamadas internamente,
+o que na verdade é um processo totalmente diferente das chamadas externas.
+As chamadas internas são executadas através de saltos no código, e os
+argumentos da matriz são passados internamente por ponteiros para a memória.
+Assim, quando o compilador gera o código para uma função interna, essa função
+espera que seus argumentos estejam localizados na memória.
+
+Para funções externas, o compilador não precisa permitir chamadas internas e,
+portanto, permite que os argumentos sejam lidos diretamente do calldata,
+salvando a etapa de cópia.
+
+Quanto às melhores práticas, você deve usar external se você espera que a
+função só será chamada externamente, e use public se você precisar chamar a
+função internamente. Quase nunca faz sentido usar o this.f() padrão, pois
+isso requer um real CALL para ser executado, o que é caro. Além disso, passar
+matrizes através deste método seria muito mais caro do que passá-los internamente.
+
+Você verá essencialmente benefícios de desempenho com external sempre que você
+está chamando apenas uma função externamente, e passando em um monte de calldata
+(por exemplo, grandes matrizes).
+
+Exemplos para diferenciar:
+
+público - todos podem acessar
+
+externo - Não pode ser acessado internamente, apenas externamente
+
+interno - somente este contrato e contratos derivados dele podem acessar
+
+privado - pode ser acessado apenas a partir deste contrato "
+(HESS, 2017)
+
+----------------------------------------------------------------
+REFERENCIA:
+
+HESS, Tjaden 
+Ethereum Stack Exchange 2017. external vs public best practices
+[Acesso em 5 Feb 2025]. Disponível em:
+https://ethereum.stackexchange.com/questions/19380/external-vs-public-best-practices
+*/
